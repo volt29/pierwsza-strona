b2a67c7eb028717037d22b830b9eb72189b4b2a7
Maciek Samoraj <maciek.samoraj@gmail.comgit config user.email maciek.samoraj@gmail.com>
2016-09-22 16:43:42 +0200

    Add grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,15 +22,37 @@ module.exports = function(grunt) {
   				dest: 'images/build/'
   			}]
   		}
-  	}
-
+  	},
+	  
+watch: {
+    scripts: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        },
+    } 
+},
+	  
+browserSync: {
+    bsFiles: {
+        src : 'css/*.css'
+    },
+    options: {
+        server: {
+            baseDir: "./"
+        }
+    }
+},	  
 
   });
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');	
 
   // Default task(s).
   
-  grunt.registerTask('default', ['sass', 'imagemin']);
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch', 'browserSync']);
 };
